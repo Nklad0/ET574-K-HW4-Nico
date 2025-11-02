5c48b53670ad64fc226b5e42e56eda13a2077a72
Nklad0 <nicholas.kladopoulos86@student.qcc.cuny.edu>
2025-11-02 20:38:34 +0000

    Added main function for console gameplay

--- main.py
+++ main.py
@@ -20,10 +20,39 @@ class DiceGame:
        result = self.evaluate_roll(total)
        return {"die1": roll1, "die2": roll2, "total": total, "result": result}
     
-    def evaulate_roll(self, total):
+    def evaluate_roll(self, total):
         if total in (7, 11):
             return "You win"
         elif total in (2, 3, 12):
             return "You lose"
         else:
             return "Please roll again"
+
+
+def main():
+    print("Welcome to the Dice Game Simulator!")
+    game = DiceGame()
+
+    while True:
+        print("\nMenu:")
+        print("1. Play a round!")
+        print("2. Exit game")
+        choice = input("Enter your choice here: ")   
+
+
+        if choice == "1":
+           outcome = game.play_round()
+           print(f"\nYou have just rolled {outcome['die1']} and {outcome['die2']}.")
+           print(f"Total: {outcome['total']}")
+           print(f"Result: {outcome['result']}")
+        elif choice == "2":
+           print("Goodbye!")
+           break
+    
+        else:
+           print("Wrong choice! Please try again!")
+
+
+
+if __name__ == "__main__":
+    main()
